363f6858fcc1e3e9046e03c08108c4f2aaeb8f1b
Dono <erik.dono@gmail.com>
2020-04-24 19:58:17 -0400

    grabbing files

---
+++ index.js
@@ -0,0 +1,25 @@
+const colors = require("colors");
+const inquirer = require("inquirer")
+const fs = require("fs")
+
+
+const questions = [
+    projectTitle = projectTitle,
+    Description = Description,
+    tableOfContents = tableOfContents,
+    Installation = Installation,
+    Usage = Usage,
+    License = License,
+    Contributing = Contributing,
+    Tests = Tests
+];
+
+function writeToFile(fileName, data) {
+}
+
+function init() {
+    console.log("This should Be green".green)
+
+}
+
+init();
