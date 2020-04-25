7b6c70e385e96cccea8f4d02d3a30b9ce38ecd47
Dono <erik.dono@gmail.com>
2020-04-24 21:19:58 -0400

    added questions

--- utils/questions.js
+++ utils/questions.js
@@ -2,7 +2,14 @@ const private = "private";
 
 module.exports = {
     start: [
-        { type: 'input', name: "name", message: "What is your name" },
-        { type: "input", name: "title", message: "What is the name of the project" }
+        { type: "input", name: "title", message: "What is the name of the project" },
+        { type: "input", name: "description", message: "Describe your Project" },
+        { type: "input", name: "tableOfContents", message: "What is the name of the project" },
+        { type: "input", name: "Installation", message: "Any Instructions for installing?" },
+        { type: "input", name: "Usage", message: "What is the project's usage" },
+        { type: "input", name: "Liscense", message: "What is the liscense" },
+        { type: "input", name: "Tests", message: "What test have you made?" },
+        { type: "input", name: "github", message: "What is the name of the project" }
+
     ]
 }
